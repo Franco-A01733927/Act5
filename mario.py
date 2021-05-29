b5074af3acb2c08bb0dd6a632aa67787373bfad3
Franco-A01733927 <67660198+Franco-A01733927@users.noreply.github.com>
2021-05-28 21:54:20 -0500

    Documented Py file

--- mario.py
+++ mario.py
@@ -1,6 +1,12 @@
-#!/usr/bin/env python
-
-
+#Authors:
+#Franco Minutti Simoni - A01733927
+#Alan Mondragón Rivas - A01734565
+#Created 20 May, 2021
+#The game moves Mario across the surface.
+#The fist is controlled using a matrix keyboard with an arduino using serial communication.
+#The objective of the game is to hit Mario that is an image of 56x56 pixels, with a fist that with the same dimensions.
+#It displays a table of points and a timer. #The game lasts 2 minutes and every 20 seconds the movement speed of Mario increases.
+#The user gets 10 points for each time he hits Mario and loses 20 if he does not hit it.
 
 #Import Modules
 import os, pygame
@@ -15,7 +21,9 @@ if not pygame.font: print ('Warning, fonts disabled')
 if not pygame.mixer: print ('Warning, sound disabled')
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
+#Change directory depending on the PC
 data_dir = os.path.join(main_dir, 'C:/Users/E5570/Documents/Tec/4to semestre/Sistemas de chip/Interfaces Python/PyGame')
+#Global variables for image location and serial port
 x = 0
 y = 0
 ser =  serial.Serial('COM6',baudrate=9600, timeout=0.005)
@@ -41,7 +49,7 @@ class Fist(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self) #call Sprite initializer
         self.image, self.rect = load_image('fist.bmp', -1)
         self.punching = 0
-
+    #Udates the coordinates of the Fist
     def update(self):
         global x,y
         "move the fist based on the mouse position"
@@ -68,7 +76,7 @@ class Chimp(pygame.sprite.Sprite):
        monkey when it is punched."""
     def __init__(self):
         pygame.sprite.Sprite.__init__(self) #call Sprite intializer
-        self.image, self.rect = load_image('mario.jpg', -1)
+        self.image, self.rect = load_image('mario.jpg', -1)#Takes the Image of the Character
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
         self.rect.topleft = 10, 10
@@ -76,7 +84,7 @@ class Chimp(pygame.sprite.Sprite):
         self.y = 4
         self.dizzy = 0
 
-
+    #The character spins
     def update(self):
         "walk or spin, depending on the monkeys state"
 
@@ -84,13 +92,12 @@ class Chimp(pygame.sprite.Sprite):
             self._spin()
         else:
             self._walk()
-
+    #Makes the character move all arround the window
     def _walk(self):
         "move the monkey across the screen, and turn at the ends"
         newpos = self.rect.move((self.move, self.y))
         if self.rect.left < self.area.left or self.rect.right > self.area.right:
             self.move = -self.move
-            #self.y =
             newpos = self.rect.move((self.move, self.y))
             self.image = pygame.transform.flip(self.image, 1, 0)
         if self.rect.top < self.area.top or self.rect.bottom > self.area.bottom:
@@ -115,14 +122,14 @@ class Chimp(pygame.sprite.Sprite):
         if not self.dizzy:
             self.dizzy = 1
             self.original = self.image
-
+    #Increases the character speed
     def moreSpeed(self):
 
         if (self.move < 0):
             self.move = ((-self.move) + 5)*-1
         else:
             self.move += 5
-        
+
 
 def main():
     """this function is called when the program starts.
@@ -169,13 +176,15 @@ def main():
 #Main Loop
     going = True
     points = 0
-    # Usado para gestionar cuán rápido se actualiza la pantalla
+    # Used to manage how fast the screen updates and our timer
     reloj = pygame.time.Clock()
-    # Esta es la fuente que usaremos para el textoo que aparecerá en pantalla (tamaño 25)
+    # This is the font that we will use for the text that will appear on the screen (size 25)
     fuente = pygame.font.Font(None, 25)
+    #Initializes the timer variables
     numero_de_fotogramas = 0
     tasa_fotogramas = 20
     instante_de_partida = 120
+    #Sets the colors
     NEGRO = (0, 0, 0)
     BLANCO = (255, 255, 255)
     tmp = 40
@@ -187,10 +196,10 @@ def main():
                 going = False
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 going = False
+        #Reads the serial port
         line = ser.readline().decode('utf-8').rstrip()
-        # if ser.in_waiting > 0:
         if line == "5":
-            #pygame.time.delay(50)
+            #Punch action
             if fist.punch(chimp):
                 punch_sound.play() #punch
                 chimp.punched()
@@ -200,42 +209,44 @@ def main():
                 points -= 20
             pygame.time.delay(100)
             fist.unpunch()
-
+        #Moves the fist depending on the key pressed on the xX axis
         if line == "6":
             x+=10
-        # Or rotates the object to the opposite direction
+
         if line == "4":
             x-=10
+        #Moves the fist depending on the key pressed on the Y axis
         if line == "8":
             y+=10
-        # Or rotates the object to the opposite direction
         if line == "2":
             y-=10
-
+        #The timer initializes
         segundos_totales = instante_de_partida - (numero_de_fotogramas // tasa_fotogramas)
         if segundos_totales < 0:
             segundos_totales = 0
 
-        # Dividimos por 60 para obtener los minutos totales
+        # We divide by 60 to get the total minutes
         minutos = segundos_totales // 60
 
-        # Usamos el módulo (resto) para obtener los segundos
+        # We use the module to obtain the seconds
         segundos = segundos_totales % 60
 
-        # Usamos el formato de cadenas de texto para formatear los ceros del principio
+        # We use the text string format to format leading zeros
         texto_de_salida = "Time left: {0:02}:{1:02}".format(minutos, segundos)
         if tmp < 0:
             tmp = 40
+        #Increments the speed every 20 seconds
         if ((segundos == 0 or segundos == 20 or segundos == 40) and segundos == tmp):
                 tmp -= 20
                 chimp.moreSpeed()
 
 
-
+        #initializes when the time is up
         if texto_de_salida == "Time left: 00:00":
             going = False
             pygame.mixer.music.pause()
             screen.fill(NEGRO)
+            #Determines if the user had won
             if points <= 0:
                 mens = fuente.render("You Lose", True, BLANCO)
                 screen.blit(mens, [170, 150])
@@ -249,12 +260,12 @@ def main():
                 end_plus_sound.play()
                 pygame.time.delay(6000)
 
-        # Volcamos en pantalla
+        # Refresh the timer
         texto = fuente.render(texto_de_salida, True, NEGRO)
 
 
 
-        # TODO EL CÓDIGO DE DIBUJO DEBERÍA IR ENCIMA DE ESTE COMENTARIO
+
 
 
 
@@ -268,7 +279,7 @@ def main():
         pygame.display.flip()
         numero_de_fotogramas += 1
 
-        # Limitamos a 20 fotogramas por segundo
+        # Set the limit to 20 frames
         reloj.tick(20)
 
     pygame.quit()
